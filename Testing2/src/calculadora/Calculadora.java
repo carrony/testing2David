a1dc60bbce12c0571e8d858babdf005fbd82a59f
David <David@DESKTOP-1VREN8U>
2023-02-10 12:24:18 +0100

    commit inicial

---
+++ Testing2/src/calculadora/Calculadora.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package calculadora;
+
+import java.util.Iterator;
+
+/**
+ * @author David
+ *
+ */
+public class Calculadora {
+
+	static float  suma (float a, float b) {
+		return a+b;
+	}
+	
+	static float divide (int a , int b) {
+		//if (b==0) return 0;
+		return a/b;
+	}
+	
+	static boolean esPar(int num) {
+		if (num%2 ==0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	static boolean esPrimo(int num) {
+		num=Math.abs(num);
+		for (int i=2; i<num;i++) {
+			if (num%i==0) return false;
+		}
+		return true;
+	}
+	
+	static public long factorial(int n) {
+        long producto=1;
+        int i;
+        if (n<0) 
+        	throw new ArithmeticException(
+        			"Debe ser positivo");
+        for (i = 2; i <= n; i++) {
+        	producto =producto* i;
+        }
+        return producto;
+    }
+	
+	static public void rotarDerecha(int v[]) {
+		int aux = v[v.length-1];
+		for (int i = v.length-1; i >=1; i--) {
+			v[i]=v[i-1];
+		}
+		v[0]=aux;
+	}
+}
